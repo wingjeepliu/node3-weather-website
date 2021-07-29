8b970caf3c279c59e23f5a9663c250fa3660d17e
Wing Jeep Lau <wlau@Wings-MBP.attlocal.net>
2021-07-28 18:14:17 -0700

    change for running at heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         const pError = document.querySelector('#error');
         const pData =  document.querySelector('#data')
         response.json().then((data) => {
